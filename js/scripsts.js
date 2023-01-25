7ef6226dad00be6641feefd754de893dd6174333
Cristian Bolaños <55451669+Cristian032019@users.noreply.github.com>
2023-01-25 12:21:38 -0500

    Add files via upload

--- js/scripsts.js
+++ js/scripsts.js
@@ -3,11 +3,22 @@
 
 const toggleMenuElement = document.getElementById("toogle-menu");
 const mainMenuElement = document.getElementById("main-menu");
+// const hamburgerWrapper = document.getElementById("toogle-menu");
 
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle("main-menu--show");
+    toggleMenuElement.classList.toggle('active'); 
 });
 
+
+
+
+
+//const hamburgerWrapper = document.querySelector('.toogle-menu');
+
+// hamburgerWrapper.addEventListener('click', () => {
+//     hamburgerWrapper.classList.toggle('active');
+// });
 // let toogle = document.getElementById("toggle-menu");
 // let  main = document.getElementById("main-menu");
 // let contador = 0;
